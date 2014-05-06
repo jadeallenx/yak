03fa6f89eaf60c1686db4469ccf99c0cb8d7d8c3
Mark Allen <mrallen1@yahoo.com>
2014-05-06 17:28:02 -0500

    Refactor Walk and helpers. Add tests.

--- filter.go
+++ filter.go
@@ -2,29 +2,123 @@ package yak
 
 import (
 	"code.google.com/p/go.net/html"
+	"fmt"
 	"log"
+	"net/url"
 )
 
 type Visit func(n *html.Node, p *Page) error
 
-func Walk(p *Page, v Visit) *Page {
+func resolveUrl(u *url.URL, p *Page) *url.URL {
+	return p.loc.ResolveReference(u)
+}
+
+func addChildPage(parent *Page, u *url.URL) {
+	ch := &Page{
+		parent: parent,
+		loc:    u,
+	}
+	parent.children = append(parent.children, ch)
+}
+
+func addAsset(parent *Page, u *url.URL, t assettype) {
+	a := &Asset{
+		atype: t,
+		loc:   resolveUrl(u, parent),
+	}
+	parent.assets = append(parent.assets, a)
+}
+
+func findAttrKey(n *html.Node, key string) (*url.URL, error) {
+	for _, a := range n.Attr {
+		if a.Key == key {
+			u, err := url.Parse(a.Val)
+			if err != nil {
+				return nil, err
+			}
+			return u, nil
+		}
+	}
+	return nil, fmt.Errorf("key '%s' not found", key)
+}
+
+func isCSS(n *html.Node) bool {
+	for _, a := range n.Attr {
+		return a.Key == "type" && a.Val == "text/css"
+	}
+	return false
+}
+
+func defaultf(n *html.Node, p *Page) error {
+	if n.Type == html.ElementNode {
+		switch n.Data {
+		case "a":
+			u, err := findAttrKey(n, "href")
+			if err != nil {
+				// We couldn't parse a URL for this anchor.
+				log.Println("WARNING:", err)
+				return nil
+			}
+			u = resolveUrl(u, p)
+			if u.Host == p.loc.Host {
+				// host is the same, child page
+				addChildPage(p, u)
+			} else {
+				// external
+				addAsset(p, u, Link)
+			}
+
+		case "img":
+			u, err := findAttrKey(n, "src")
+			if err != nil {
+				// We couldn't parse a URL for this image.
+				log.Println("WARNING:", err)
+				return nil
+			}
+			addAsset(p, u, Image)
+		case "script":
+			u, _ := findAttrKey(n, "src")
+			if u == nil {
+				// This script has no src, so it must be inline
+				a := &Asset{atype: Script}
+				p.assets = append(p.assets, a)
+				return nil
+			}
+			addAsset(p, u, Script)
+		case "link":
+			if isCSS(n) {
+				u, _ := findAttrKey(n, "href")
+				if u == nil {
+					// This link has no href, so it must be inline
+					a := &Asset{atype: CSS}
+					p.assets = append(p.assets, a)
+					return nil
+				}
+				addAsset(p, u, CSS)
+			}
+		}
+	}
+	return nil
+}
+
+func Walk(p *Page, v Visit) error {
 	if p.c == nil {
 		log.Println("WARNING: no html nodes found")
-		return p
+		return nil
 	}
 
 	for ch := p.c.FirstChild; ch != nil; ch = ch.NextSibling {
 		err := v(ch, p)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		err = walk(ch, p, v)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 	}
 
-	return p
+	return nil
 
 }
 
--- filter_test.go
+++ filter_test.go
@@ -2,14 +2,55 @@ package yak
 
 import (
 	"code.google.com/p/go.net/html"
-	"fmt"
+	"net/url"
 	"strings"
 	"testing"
 )
 
-func TestParse(t *testing.T) {
-	s := `<p>Links:</p><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul>`
+func TestWalk(t *testing.T) {
+	s := `<link type="text/css" rel="stylesheet" href="style.css"><p>Links:</p><img src="qux.jpg"><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul><a href="#">lala</a>`
 
-	c, _ := html.Parse(strings.NewReader(s))
-	fmt.Println(c)
+	p := &Page{
+		r:        strings.NewReader(s),
+		parent:   nil,
+		children: nil,
+		assets:   nil,
+	}
+
+	p.c, _ = html.Parse(p.r)
+	p.loc, _ = url.Parse("https://test.com/")
+
+	err := Walk(p, defaultf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(p.children) != 3 {
+		t.Fatalf("Expected child length of 3, got %v", len(p.children))
+	}
+
+	if len(p.assets) != 2 {
+		t.Fatalf("Expected assets length of 1, got %v", len(p.assets))
+	}
+
+}
+
+func TestBadWalkInput(t *testing.T) {
+	s := `<a>Degenerate Input</a><img><script>Fail whale</script>`
+
+	p := &Page{
+		r:        strings.NewReader(s),
+		parent:   nil,
+		children: nil,
+		assets:   nil,
+	}
+
+	p.c, _ = html.Parse(p.r)
+	p.loc, _ = url.Parse("https://test.com/")
+
+	err := Walk(p, defaultf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
