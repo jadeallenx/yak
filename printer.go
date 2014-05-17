cfc578df1c02849aadf780604e6b9f8741048156
Mark Allen <mrallen1@yahoo.com>
2014-05-17 02:44:35 -0500

    Add pretty printer

---
+++ printer.go
@@ -0,0 +1,40 @@
+package yak
+
+import (
+	"fmt"
+	"strings"
+)
+
+func aType2String(t Assettype) string {
+	switch t {
+	case Image:
+		return "Image"
+	case Link:
+		return "External Link"
+	case CSS:
+		return "Stylesheet"
+	case Script:
+		return "Javascript"
+	}
+	return "Unknown"
+}
+
+func PrettyPrint(p *Page, generation int) {
+	fmt.Println(strings.Repeat(">", generation) + " " + p.Loc.String())
+	fmt.Println(strings.Repeat(" ", generation) + "Assets:")
+	if len(p.Assets) > 0 {
+		for _, a := range p.Assets {
+			if a.Loc != nil {
+				fmt.Println(strings.Repeat("  ", generation) + aType2String(a.Atype) + " (" + a.Loc.String() + ")")
+			} else {
+				fmt.Println(strings.Repeat("  ", generation) + aType2String(a.Atype))
+			}
+		}
+	} else {
+		fmt.Println(strings.Repeat("  ", generation) + "None")
+	}
+	generation++
+	for _, ch := range p.Children {
+		PrettyPrint(ch, generation)
+	}
+}
--- yak.go
+++ yak.go
@@ -2,8 +2,11 @@ package yak
 
 import (
 	"code.google.com/p/go.net/html"
+	"fmt"
 	"io"
+	"log"
 	"net/url"
+	"os"
 )
 
 type assettype int
@@ -27,4 +30,51 @@ type Page struct {
 	loc      *url.URL
 	children []*Page
 	assets   []*Asset
+}
+
+func process(q *Queue) error {
+	if q.Empty() {
+		return nil
+	}
+
+	p := q.Dequeue().(*Page)
+	err := GetPage(p)
+	if err != nil {
+		return err
+	}
+
+	err = Walk(p, defaultf)
+	if err != nil {
+		return err
+	}
+
+	for _, ch := range p.children {
+		q.Enqueue(ch)
+	}
+
+	process(q)
+
+	return nil
+}
+
+func main() {
+	q := NewQueue()
+	root := &Page{}
+	var err error
+	for _, rawurl := range os.Args {
+		fmt.Println(rawurl)
+		root.loc, err = url.Parse(rawurl)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		q.Enqueue(root)
+	}
+
+	err = process(q)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//pretty print p
 }
